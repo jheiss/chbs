7d53de9dab84c93119483ee3755f48e3008e19bc
Jason Heiss <jheiss@aput.net>
2012-10-04 17:50:58 -0400

    Add some constants and included_corpora method

--- lib/chbs.rb
+++ lib/chbs.rb
@@ -2,4 +2,10 @@ require "chbs/version"
 require "chbs/generator"
 
 module Chbs
+  CORPORA_DIRECTORY = File.expand_path('../../corpora/', __FILE__)
+  DEFAULT_CORPUS = 'tv-and-movies'
+  
+  def self.included_corpora
+    Dir.glob(File.join(CORPORA_DIRECTORY, '*.json')).collect{|c| File.basename(c, '.json')}.sort
+  end
 end
