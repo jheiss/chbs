6536e32b4f76ce2526570454192786e5f56db1bb
Jason Heiss <jheiss@aput.net>
2012-10-04 21:18:34 -0400

    Test top level Chbs module

---
+++ test/test_chbs.rb
@@ -0,0 +1,21 @@
+#
+# Test Chbs
+#
+
+require 'test/unit'
+require 'chbs'
+require 'json'
+
+class ChbsTests < Test::Unit::TestCase
+  def test_constants
+    assert_nothing_raised { Chbs::CORPORA_DIRECTORY }
+    assert_kind_of String, Chbs::CORPORA_DIRECTORY
+    assert_nothing_raised { Chbs::DEFAULT_CORPUS }
+    assert_kind_of String, Chbs::DEFAULT_CORPUS
+  end
+  
+  def test_included_corpora
+    assert_equal ['gutenberg', 'tv-and-movies'], Chbs.included_corpora
+  end
+end
+
